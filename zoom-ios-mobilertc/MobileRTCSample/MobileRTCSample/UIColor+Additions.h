a81f8192270ed2f72d2a447e5c8550cb6c576e77
zoomvideo <thiya.ramalingam@zoom.us>
2016-11-04 17:40:35 -0700

    update

---
+++ zoom-ios-mobilertc/MobileRTCSample/MobileRTCSample/UIColor+Additions.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Additions.h
+//  ZoomSDKSample
+//
+//  Created by Xiaojian Hu on 3/17/14.
+//  Copyright (c) 2014 Zoom Video Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Additions)
+
++ (UIColor*)colorWithHex:(NSUInteger)hexColor;
+
+@end
