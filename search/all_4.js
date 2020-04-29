b9a9c4b32443bc089befdc1de66ecfc0ec8560cf
Carson Chen <carson.chen@zoom.us>
2020-04-29 02:42:28 -0700

    Update SDK Documentation

--- search/all_4.js
+++ search/all_4.js
@@ -3,6 +3,7 @@ var searchData=
   ['editmeeting_3a',['editMeeting:',['../interface_mobile_r_t_c_premeeting_service.html#a2d0066c52e6635769ac0bf878c6a11e6',1,'MobileRTCPremeetingService']]],
   ['enablecustommeeting',['enableCustomMeeting',['../interface_mobile_r_t_c_meeting_settings.html#a67698df806ac19d97367d891547131b3',1,'MobileRTCMeetingSettings']]],
   ['enablegreenvb_3a',['enableGreenVB:',['../category_mobile_r_t_c_meeting_service_07_virtual_background_08.html#ad2ae01597a6c4113378e8b88e59f1bb6',1,'MobileRTCMeetingService(VirtualBackground)::enableGreenVB:()'],['../interface_mobile_r_t_c_meeting_service.html#ad2ae01597a6c4113378e8b88e59f1bb6',1,'MobileRTCMeetingService::enableGreenVB:()']]],
+  ['enablekubi',['enableKubi',['../interface_mobile_r_t_c_meeting_settings.html#aaaf6e97a22eb1f84038abba501231656',1,'MobileRTCMeetingSettings']]],
   ['enablelanguageinterpretation_3a',['enableLanguageInterpretation:',['../protocol_mobile_r_t_c_meeting_item_01-p.html#aaba45f7364fc64761c9b42310c0b238f',1,'MobileRTCMeetingItem -p']]],
   ['enablelog',['enableLog',['../interface_mobile_r_t_c_s_d_k_init_context.html#a01cc3e06f0af13589a49df2287c75b30',1,'MobileRTCSDKInitContext']]],
   ['enablemeetingtopublic_3a',['enableMeetingToPublic:',['../protocol_mobile_r_t_c_meeting_item_01-p.html#a6d602f29dcbe5a00c15b7b1dcc3d5f13',1,'MobileRTCMeetingItem -p']]],
--- search/properties_4.js
+++ search/properties_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['enablecustommeeting',['enableCustomMeeting',['../interface_mobile_r_t_c_meeting_settings.html#a67698df806ac19d97367d891547131b3',1,'MobileRTCMeetingSettings']]],
+  ['enablekubi',['enableKubi',['../interface_mobile_r_t_c_meeting_settings.html#aaaf6e97a22eb1f84038abba501231656',1,'MobileRTCMeetingSettings']]],
   ['enablelog',['enableLog',['../interface_mobile_r_t_c_s_d_k_init_context.html#a01cc3e06f0af13589a49df2287c75b30',1,'MobileRTCSDKInitContext']]]
 ];
